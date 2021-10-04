a0c8d85a739510e39a7d205ea63ebf3269781ea1
riejan <eguitariejan@gmail.com>
2021-10-04 17:03:06 +0800

    v.0.0.0.1
    Create Controller for registration
    Create blade for registration
    Create layout

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,9 @@
+@extends('layout.app')
+
+@section('content')
+    <div class="flex justify-center">
+        <div class="bg-white w-4/12 p-6 rounded-lg">
+            Register
+        </div>
+    </div>
+@endsection
---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layout.app')
+
+@section('content')
+    <div class="flex justify-center">
+        <div class="bg-white w-8/12 p-6 rounded-lg">
+            Posts
+        </div>
+    </div>
+@endsection
