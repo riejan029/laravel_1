938c6744ab064e4a564da019afd394e3f0045ea8
riejan <eguitariejan@gmail.com>
2021-10-05 15:05:47 +0800

    v.0.0.0.2
    Add post
    Add relationship between user and posts
    Display Posts

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,9 +1,27 @@
 @extends('layout.app')
-
 @section('content')
-    <div class="flex justify-center">
-        <div class="bg-white w-8/12 p-6 rounded-lg">
-            Posts
-        </div>
+<div class="flex justify-center">
+    <div class="bg-white w-8/12 p-6 rounded-lg">
+        <form action="{{route('posts')}}" method="post">
+            @csrf
+            <div class="mb-4">
+                <textarea class="w-full bg-gray-100 border-2 p-4 rounded-lg" name="body" id="body" placeholder="Post something..." cols="30" rows="4"></textarea>
+            </div>
+            <div>
+                <button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded font-medium">Post</button>
+            </div>
+        </form>
+
+        @if ($posts->count())
+            @foreach ($posts as $post)
+                <div class="mb-4">
+                    <a href="" class="font-bold">{{$post->user->name}}</a><span class="text-gray-600 text-sm">{{$post->created_at->diffForHumans()}}</span>
+                    <p class="mb-2">{{$post->body}}</p>
+                </div>
+            @endforeach
+        @else
+            <p>There is no posts</p>
+        @endif
     </div>
+</div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\LogoutController;
@@ -10,9 +11,6 @@ Route::get('/', function(){
     return view('home');
 })->name('home');
 
-Route::get('/posts', function(){
-    return view('posts.index');
-})->name('posts');
 
 Route::get('/register',[RegisterController::class,'index'])->name('register');
 Route::post('/register',[RegisterController::class,'store']);
@@ -23,3 +21,6 @@ Route::post('/login',[LoginController::class,'login']);
 Route::post('/logout',[LogoutController::class,'logout'])->name('logout');
 
 Route::get('/dashboard',[DashboardController::class,'index'])->name('dashboard');
+
+Route::get('/posts',[PostController::class,'index'])->name('posts');
+Route::post('/posts',[PostController::class,'addPost']);
